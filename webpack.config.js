3c6c8d6589765dc40b97707efbb70bea55182747
Albert <albertDavo>
2021-04-22 14:56:44 +0400

    project

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   entry: path.resolve(__dirname, './src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+
     filename: '[fullhash][name].js',
   },
 
